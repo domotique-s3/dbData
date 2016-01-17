efff61904cf27ed5993acc8c4477acbcf528f994
Sebastien Klasa <skeggib@gmail.com>
2016-01-17 19:32:41 +0100

    [Back][FilePDoBuilderException] Fixed #17 - Added FilePDOBuilderException

--- src/DS3/Application/FilePDOBuilder.php
+++ src/DS3/Application/FilePDOBuilder.php
@@ -3,6 +3,7 @@
 namespace DS3\Application;
 
 use DS3\Framework\Filesystem\File;
+use DS3\ApplicationFilePDOBuilderException\FilePDOBuilderException;
 
 class FilePDOBuilder extends PDOBuilder {
 
@@ -12,17 +13,14 @@ class FilePDOBuilder extends PDOBuilder {
 
 	public function __construct($path) {
 		if (!File::exists($path))
-			throw new \Exception("Configuration file does not exists");
+			throw new FilePDOBuilderException("Configuration file does not exists");
 		$this->file = new File($path);
 		$str = $this->file->read();
-
-		if ($str == "")
-			throw new \Exception("$path is empty");
 
 		$array = explode("\n", $str);
 
 		if (count($array) < 5)
-			throw new \Exception("Configuration file does not contains all required informations");
+			throw new FilePDOBuilderException("Configuration file does not contains all required informations");
 
 		$this->driver = $array[0];
 		$this->database_name = $array[1];
--- tests/DS3/Application/FilePDOBuilderTest.php
+++ tests/DS3/Application/FilePDOBuilderTest.php
@@ -7,7 +7,7 @@ class FilePDOBuilderTest extends \PHPUnit_Framework_TestCase
 	public function testGet() {
 		$config = new FilePDOBuilder("pdo_unittests.cfg");
 
-		$this->assertEquals('pgsql', $config->getConnector());
+		$this->assertEquals('pgsql', $config->getDriver());
 		$this->assertEquals('dbcharts', $config->getDatabaseName());
 		$this->assertEquals('localhost', $config->getHost());
 		$this->assertEquals('user', $config->getLogin());
