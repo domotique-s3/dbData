556724d42832c648fee7b906e34b292884f467a6
Loïc Payol <gamer204@gmail.com>
2016-01-17 21:30:55 +0100

    [Back][PDO] Fixed tests

--- tests/DS3/Framework/PDO/FilePDOBuilderTest.php
+++ tests/DS3/Framework/PDO/FilePDOBuilderTest.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace DS3\Application;
+namespace DS3\Framework\PDO;
 
 class FilePDOBuilderTest extends \PHPUnit_Framework_TestCase
 {
     public function testGet()
     {
-        $config = new FilePDOBuilder(__DIR__.'/pdo_unittests.cfg');
+        $config = new FilePDOBuilder(__DIR__ . '/pdo_unittests.cfg');
 
 		$this->assertEquals('pgsql', $config->getDriver());
 		$this->assertEquals('dbcharts', $config->getDatabaseName());
 		$this->assertEquals('localhost', $config->getHost());
-		$this->assertEquals('user', $config->getLogin());
+		$this->assertEquals('user', $config->getUsername());
 		$this->assertEquals('pass', $config->getPassword());
 	}
 }
