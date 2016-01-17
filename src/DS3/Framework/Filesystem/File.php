f41089b77b95ba6617763460bce0d25c0a5359c0
Loïc Payol <gamer204@gmail.com>
2016-01-17 21:30:44 +0100

    [Back][Filesystem] Added FilesystemException, fixes #10

--- src/DS3/Framework/Filesystem/File.php
+++ src/DS3/Framework/Filesystem/File.php
@@ -2,28 +2,35 @@
 
 namespace DS3\Framework\Filesystem;
 
+use DS3\Framework\Filesystem\Exception\FilesystemException;
+
 /**
  * Enable read/write in a file.
  */
 class File
 {
-    /* --- ATTRIBUTES --- */
-
+    /**
+     * @var resource The file resource
+     */
     private $handle;
     private $path;
 
-    /* --- CONSTRUCTORS --- */
-
-    /*!
-     * Path constructor, open file in r/w
+    /**
+     * Opens the given file, or create it if it doesn't exists
+     *
      * @param string $path File path
      */
     public function __construct($path)
     {
         $this->path = $path;
-        $this->handle = fopen($path, 'c+');
-        $filesize = filesize($path);
-        fseek($this->handle, $filesize);
+
+        try {
+            $this->handle = fopen($this->path, 'c+');
+            $filesize = filesize($this->path);
+            fseek($this->handle, $filesize);
+        } catch (\Exception $e) {
+            throw new FilesystemException("Cannot open file {$this->path}", 0, $e);
+        }
     }
 
     public function __destruct()
@@ -31,43 +38,56 @@ class File
         fclose($this->handle);
     }
 
-    /* --- METHODS --- */
-
-    /*!
+    /**
      * Write at the end of the file
+     *
      * @param  string $str Text to write
      * @return void
+     * @throws FilesystemException When a writing error occurs
      */
     public function write($str)
     {
         if (!fwrite($this->handle, $str)) {
-            throw new \Exception('Cannot write file '.$this->path);
+            throw new FilesystemException("Cannot write file {$this->path}");
         }
     }
 
-    /*!
-     * Read file
-     * @return string File contents
+    /**
+     * Read the file and returns its content.
+     *
+     * @return string The file contents
+     * @throws FilesystemException When a read error occurs
      */
     public function read()
     {
-        fseek($this->handle, 0);
+        try {
+            fseek($this->handle, 0);
 
-        $contents = '';
-        while (!feof($this->handle)) {
-            $contents .= fread($this->handle, 1024);
+            $contents = '';
+            while (!feof($this->handle)) {
+                $contents .= fread($this->handle, 1024);
+            }
+
+            return $contents;
+        } catch(\Exception $e) {
+            $this->__destruct();
+            throw new FilesystemException("Cannot read file {$this->path}", 0, $e);
         }
-
-        return $contents;
     }
 
-    /*!
+    /**
      * Delete file contents
+     *
      * @return void
+     * @throws FilesystemException When a writing error occurs
      */
     public function clear()
     {
-        file_put_contents($this->path, '');
+        try {
+            file_put_contents($this->path, '');
+        } catch(\Exception $e) {
+            throw new FilesystemException("Cannot read file {$this->path}", 0, $e);
+        }
     }
 
     public static function exists($path)
