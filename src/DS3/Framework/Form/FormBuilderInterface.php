246adf0b90ec0377df4705bd40c6b96fe262ed49
Loïc Payol <gamer204@gmail.com>
2016-01-24 18:56:35 +0100

    [Back][Form] Added FormBuilderInterface

---
+++ src/DS3/Framework/Form/FormBuilderInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DS3\Framework\Form;
+
+interface FormBuilderInterface
+{
+    /**
+     * @param object $model The model to bind to the form
+     * @return Form
+     */
+    public function buildForm(&$model);
+}
