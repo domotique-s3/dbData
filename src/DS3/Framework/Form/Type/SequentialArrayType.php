66109ff9af7ee89da93aa0e7acc5c502375c4fec
Loïc Payol <gamer204@gmail.com>
2016-01-24 19:40:25 +0100

    [Back][Form] Added SequentialArrayType

---
+++ src/DS3/Framework/Form/Type/SequentialArrayType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DS3\Framework\Form\Type;
+
+
+class SequentialArrayType implements TypeInterface
+{
+    /**
+     * @var TypeInterface
+     */
+    private $valuesType;
+
+    /**
+     * SequentialArrayType constructor.
+     * @param TypeInterface $valuesType
+     */
+    public function __construct(TypeInterface $valuesType)
+    {
+        $this->valuesType = $valuesType;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function transform($value)
+    {
+        return (new AssociativeArrayType(new IntegerType(), $this->valuesType))
+            ->transform($value);
+    }
+}
---
+++ tests/DS3/Framework/Form/Type/SequentialArrayTypeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DS3\Framework\Form\Type;
+
+
+class SequentialArrayTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValid()
+    {
+        $t = new SequentialArrayType(new DoubleType());
+        $this->assertSame(
+            array(5.0, 2.5, 4.0),
+            $t->transform(array('5', '2.5', 4))
+        );
+    }
+}
